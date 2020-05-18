10a2cdec809091114c1f833681a44b6707d03358
ygorferreira4g <65570661+ygorferreira4g@users.noreply.github.com>
2020-05-18 20:12:05 -0300

    Ajuste link

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,7 +6,7 @@ const routes = Router();
 routes.post('/Devs', async (request, response) => {
     const { github_username } = request.body;
 
-    const apiresponse = await axios.get(`https://api.github.com/user/${github_username}`);
+    const apiresponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
     Console.log(response.data);
     
