93d84f89e860f2974620280eeaa80494735eccd7
Álvaro Ribeiro Pereira <59286021+alvaroico@users.noreply.github.com>
2020-05-19 20:33:06 -0300

    Ajuste mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,8 @@ const app = express();
 mongoose.connect('mongodb+srv://35243625:35243625@cluster0-hcomv.mongodb.net/aps10?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+  useNewUrlParser: true
 });
 
 app.use(express.json());
