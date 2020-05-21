55dcdcc4b9395dd9a3e52c15bbef4401132c4740
Álvaro Ribeiro Pereira <59286021+alvaroico@users.noreply.github.com>
2020-05-20 21:47:58 -0300

    Formulario OK

--- Formulario/scripts.js
+++
@@ -1,8 +0,0 @@
-const currentPage = location.pathname
-const menuItens = document.querySelectorAll("header .links a")
-
-for (item of menuItens){
-    if (currentPage.includes(item.getAttribute("href"))){
-        item.classList.add("active")
-    }
-}
