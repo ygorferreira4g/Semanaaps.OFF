45d8cb3b5fdf493c76b51c6eb22470f9316ed04f
ygorferreira4g <65570661+ygorferreira4g@users.noreply.github.com>
2020-05-24 21:31:07 -0300

    APP

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://35243625:35243625@cluster0-hcomv.mongodb.net/aps
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
